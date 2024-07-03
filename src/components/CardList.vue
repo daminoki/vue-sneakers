beba4ec156b606c6554360c34c503740a8fb1ad3
Daria Andreeva <d.andreeva@grechka.digital>
2024-07-03 17:16:28 +0400

    add search logic

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -4,17 +4,28 @@ import { IProduct } from '@/entities/products.ts';
 
 defineProps<({
   products: IProduct[],
+  isProductsLoading: boolean,
 })>();
 </script>
 
 <template>
-  <div class="p-5">
-    <ul class="grid grid-cols-4 gap-4 max-tablet:grid-cols-3 max-pad:grid-cols-1">
-      <li v-for="item in products" :key="item.id">
-        <ProductCard :product="item" />
-      </li>
-    </ul>
+  <div v-if="!products.length && !isProductsLoading" class="py-48 my-auto max-pad:py-24">
+    <img
+      src="/emoji-1.png"
+      width="70"
+      height="70"
+      class="mx-auto"
+    >
+    <p class="text-center text-2xl font-bold mt-5">
+      Ничего не найдено
+    </p>
   </div>
+
+  <ul v-else class="grid grid-cols-4 gap-4 max-tablet:grid-cols-3 max-pad:grid-cols-1">
+    <li v-for="item in products" :key="item.id">
+      <ProductCard :product="item" />
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
 import CardList from '@/components/CardList.vue';
 import { getProducts } from '@/api/index.ts';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { IProduct } from '@/entities/products.ts';
 import useFavoritesStore from '@/stores/favorites.ts';
 
 const products = ref<IProduct[]>([]);
+const isProductsLoading = ref(false);
 
 const fetchProducts = async () => {
+  isProductsLoading.value = true;
   const { data } = await getProducts();
+  isProductsLoading.value = false;
 
   if (data) {
     products.value = data;
@@ -21,12 +24,29 @@ onMounted(() => {
   favoritesStore.initializeFavorites();
   fetchProducts();
 });
+
+const searchQuery = ref('');
+
+const searchedProducts = computed(
+  () => products.value.filter((product) => product.title.toLowerCase().includes(searchQuery.value.toLowerCase())),
+);
 </script>
 
 <template>
-  <div>
-    <CardList :products="products" />
+  <div class="flex justify-end pb-8">
+    <div class="relative max-pad:w-full">
+      <img src="/search.svg" alt="search" class="absolute top-1/2 left-3 w-4 h-4 -translate-y-1/2">
+      <input
+        v-model="searchQuery"
+        placeholder="Поиск"
+        type="search"
+        class="no-search-cancel p-3 pl-10 rounded-3xl border-border-color border-2
+    min-w-80 focus:outline-none focus:border-text-light
+    transition-colors duration-300 ease-in-out text-text-light-secondary max-pad:w-full max-pad:min-w-full"
+      >
+    </div>
   </div>
+  <CardList :products="searchedProducts" :is-products-loading="isProductsLoading" />
 </template>
 
 <style scoped></style>
